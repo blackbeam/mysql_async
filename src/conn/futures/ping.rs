ec670cd181fc26d0cf6a1966e9851d929493f8e9
blackbeam <aikorsky@gmail.com>
2016-11-07 17:40:51 +0300

    Use steps! for Ping future

--- src/conn/futures/ping.rs
+++ src/conn/futures/ping.rs
@@ -6,38 +6,19 @@ use lib_futures::Async;
 use lib_futures::Async::Ready;
 use lib_futures::Future;
 use lib_futures::Poll;
-use proto::Packet;
 use proto::PacketType;
 
 
-enum Step {
-    WritePacket(WritePacket),
-    ReadResponse(ReadPacket),
-}
-
-enum Out {
-    WritePacket(Conn),
-    ReadResponse((Conn, Packet)),
-}
-
-pub struct Ping {
-    step: Step,
+steps! {
+    Ping {
+        WritePacket(WritePacket),
+        ReadPacket(ReadPacket),
+    }
 }
 
 /// Future that resolves to `Conn` if MySql's `COM_PING` was executed successfully.
-impl Ping {
-    fn either_poll(&mut self) -> Result<Async<Out>> {
-        match self.step {
-            Step::WritePacket(ref mut fut) => {
-                let val = try_ready!(fut.poll());
-                Ok(Ready(Out::WritePacket(val)))
-            },
-            Step::ReadResponse(ref mut fut) => {
-                let val = try_ready!(fut.poll());
-                Ok(Ready(Out::ReadResponse(val)))
-            },
-        }
-    }
+pub struct Ping {
+    step: Step,
 }
 
 pub fn new(write_packet: WritePacket) -> Ping {
@@ -53,10 +34,10 @@ impl Future for Ping {
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
         match try_ready!(self.either_poll()) {
             Out::WritePacket(conn) => {
-                self.step = Step::ReadResponse(conn.read_packet());
+                self.step = Step::ReadPacket(conn.read_packet());
                 self.poll()
             },
-            Out::ReadResponse((conn, packet)) => {
+            Out::ReadPacket((conn, packet)) => {
                 if packet.is(PacketType::Ok) {
                     Ok(Ready(conn))
                 } else {
