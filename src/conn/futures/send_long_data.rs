7a6ce422e6086c78010682cd403eb2065bd4dfa8
blackbeam <aikorsky@gmail.com>
2016-11-07 22:23:02 +0300

    Use steps! for SendLongData future

--- src/conn/futures/send_long_data.rs
+++ src/conn/futures/send_long_data.rs
@@ -7,6 +7,8 @@ use consts::Command;
 use errors::*;
 use lib_futures::Async;
 use lib_futures::Async::Ready;
+use lib_futures::Done;
+use lib_futures::done;
 use lib_futures::Future;
 use lib_futures::Poll;
 use std::io::Write;
@@ -15,14 +17,11 @@ use value::Value;
 use value::Value::*;
 
 
-enum Step {
-    WritePacket(WritePacket),
-    Done(Option<Conn>),
-}
-
-enum Out {
-    WritePacket(Conn),
-    Done(Conn, InnerStmt),
+steps! {
+    SendLongData {
+        Init(Done<Conn, Error>),
+        WritePacket(WritePacket),
+    }
 }
 
 /// Future that sends part of statement parametes via MySql's `COM_STMT_SEND_LONG_DATA`.
@@ -30,73 +29,19 @@ enum Out {
 /// It resolves to `Conn`, `InnerStmt` and statement parameters.
 pub struct SendLongData {
     step: Step,
-    stmt: Option<InnerStmt>,
+    inner_stmt: Option<InnerStmt>,
     params: Vec<Value>,
     ids: Vec<u16>,
     next_chunk: usize,
 }
 
-impl SendLongData {
-    fn either_poll(&mut self) -> Result<Async<Out>> {
-        match self.step {
-            Step::WritePacket(ref mut fut) => {
-                Ok(Ready(Out::WritePacket(try_ready!(fut.poll()))))
-            },
-            Step::Done(ref mut conn) => {
-                let conn = conn.take().unwrap();
-                let stmt = self.stmt.take().unwrap();
-                Ok(Ready(Out::Done(conn, stmt)))
-            }
-        }
-    }
-}
-
-pub fn new(conn: Conn, stmt: InnerStmt, params: Vec<Value>, mut ids: Vec<u16>) -> SendLongData {
-    let data = match ids.pop() {
-        Some(id) => match params[id as usize] {
-            Bytes(ref x) => {
-                let max_allowed_packet = conn.max_allowed_packet as usize;
-                let mut chunks = x.chunks(max_allowed_packet - 8);
-                match chunks.next() {
-                    Some(chunk) => {
-                        let mut buf = Vec::with_capacity(chunk.len() + 7);
-                        buf.write_u32::<LE>(stmt.statement_id).unwrap();
-                        buf.write_u16::<LE>(id).unwrap();
-                        buf.write_all(chunk).unwrap();
-                        if chunk.len() < max_allowed_packet - 8 {
-                            Some((buf, 0))
-                        } else {
-                            ids.push(id);
-                            Some((buf, 1))
-                        }
-                    },
-                    None => Some((vec![], 0)),
-                }
-            },
-            _ => unreachable!(),
-        },
-        None => None,
-    };
-    match data {
-        Some((data, next_chunk)) => {
-            let future = conn.write_command_data(Command::COM_STMT_SEND_LONG_DATA, data);
-            SendLongData {
-                step: Step::WritePacket(future),
-                stmt: Some(stmt),
-                params: params,
-                ids: ids,
-                next_chunk: next_chunk,
-            }
-        },
-        None => {
-            SendLongData {
-                step: Step::Done(Some(conn)),
-                stmt: Some(stmt),
-                params: vec![],
-                ids: vec![],
-                next_chunk: 0
-            }
-        }
+pub fn new(conn: Conn, inner_stmt: InnerStmt, params: Vec<Value>, ids: Vec<u16>) -> SendLongData {
+    SendLongData {
+        step: Step::Init(done(Ok(conn))),
+        inner_stmt: Some(inner_stmt),
+        params: params,
+        ids: ids,
+        next_chunk: 0
     }
 }
 
@@ -106,12 +51,12 @@ impl Future for SendLongData {
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
         match try_ready!(self.either_poll()) {
-            Out::WritePacket(conn) => {
+            Out::Init(conn) | Out::WritePacket(conn) => {
                 let data = match self.ids.pop() {
                     Some(id) => match self.params[id as usize] {
                         Bytes(ref x) => {
                             let max_allowed_packet = conn.max_allowed_packet as usize;
-                            let stmt_id = self.stmt.as_ref().unwrap().statement_id;
+                            let stmt_id = self.inner_stmt.as_ref().unwrap().statement_id;
                             let mut chunks = x.chunks(max_allowed_packet - 8);
                             match chunks.nth(self.next_chunk) {
                                 Some(chunk) => {
@@ -126,7 +71,10 @@ impl Future for SendLongData {
                                     }
                                 },
                                 None => {
-                                    Some((vec![], 0, None))
+                                    let mut buf = Vec::with_capacity(8);
+                                    buf.write_u32::<LE>(stmt_id).unwrap();
+                                    buf.write_u16::<LE>(id).unwrap();
+                                    Some((buf, 0, None))
                                 }
                             }
                         },
@@ -145,14 +93,11 @@ impl Future for SendLongData {
                         self.poll()
                     },
                     None => {
-                        self.step = Step::Done(Some(conn));
-                        self.poll()
+                        let stmt = self.inner_stmt.take().unwrap();
+                        let params = mem::replace(&mut self.params, vec![]);
+                        Ok(Ready((conn, stmt, params)))
                     }
                 }
-            },
-            Out::Done(conn, stmt) => {
-                let params = mem::replace(&mut self.params, vec![]);
-                Ok(Ready((conn, stmt, params)))
             },
         }
     }
