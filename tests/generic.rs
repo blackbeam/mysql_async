b9bb482691dfd6024e8ac8ba02db7e2c0b308159
blackbeam <aikorsky@gmail.com>
2019-04-20 14:29:13 +0300

    Cargo fmt

--- tests/generic.rs
+++ tests/generic.rs
@@ -7,9 +7,9 @@
 // modified, or distributed except according to those terms.
 
 use ::futures::Future;
-use mysql_async::{MyFuture, Pool, QueryResult, Opts};
 use mysql_async::error::Error;
 use mysql_async::prelude::*;
+use mysql_async::{MyFuture, Opts, Pool, QueryResult};
 
 use std::env;
 use std::io;
@@ -17,10 +17,10 @@ use std::io;
 /// Same as `tokio::run`, but will panic if future panics and will return the result
 /// of future execution.
 fn run<F, T, U>(future: F) -> Result<T, U>
-    where
-        F: Future<Item = T, Error = U> + Send + 'static,
-        T: Send + 'static,
-        U: Send + 'static,
+where
+    F: Future<Item = T, Error = U> + Send + 'static,
+    T: Send + 'static,
+    U: Send + 'static,
 {
     let mut runtime = tokio::runtime::Runtime::new().unwrap();
     let result = runtime.block_on(future);
@@ -44,30 +44,35 @@ fn get_url() -> String {
     }
 }
 
-pub fn get_all_results<TupleType, T, P>(result : QueryResult<T, P>) -> impl MyFuture<Vec<TupleType>>
-    where TupleType: FromRow + Send + 'static,
-          P: Protocol + Send + 'static,
-          T: ConnectionLike + Sized + Send + 'static,  {
-    result.collect()
-        .map(|(_, data)| data)
+pub fn get_all_results<TupleType, T, P>(result: QueryResult<T, P>) -> impl MyFuture<Vec<TupleType>>
+where
+    TupleType: FromRow + Send + 'static,
+    P: Protocol + Send + 'static,
+    T: ConnectionLike + Sized + Send + 'static,
+{
+    result.collect().map(|(_, data)| data)
 }
 
-pub fn get_single_result<TupleType, T, P>(result : QueryResult<T, P>) -> impl MyFuture<TupleType>
-    where TupleType: FromRow + Send + 'static,
-          P: Protocol + Send + 'static,
-          T: ConnectionLike + Sized + Send + 'static,  {
-    get_all_results(result)
-        .and_then(|mut data| if data.len() != 1 {
+pub fn get_single_result<TupleType, T, P>(result: QueryResult<T, P>) -> impl MyFuture<TupleType>
+where
+    TupleType: FromRow + Send + 'static,
+    P: Protocol + Send + 'static,
+    T: ConnectionLike + Sized + Send + 'static,
+{
+    get_all_results(result).and_then(|mut data| {
+        if data.len() != 1 {
             Err(Error::from(io::Error::from(io::ErrorKind::InvalidData)))
         } else {
             Ok(data.remove(0))
-        })
+        }
+    })
 }
 
 #[test]
 fn use_generic_code() {
     let pool = Pool::new(Opts::from_url(&*get_url()).unwrap());
-    let fut = pool.get_conn()
+    let fut = pool
+        .get_conn()
         .and_then(move |conn| conn.query("SELECT 1, 2, 3"))
         .and_then(get_single_result::<(u8, u8, u8), _, _>);
 
