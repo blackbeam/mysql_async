5376cd8abfc190a17f9c3147051726a36dda67e1
Anatoly Ikorsky <aikorsky@gmail.com>
2020-05-06 22:58:25 +0300

    Implement Conn::get_binlog_stream

--- src/conn/mod.rs
+++ src/conn/mod.rs
@@ -869,14 +869,317 @@ impl Conn {
         }
         Ok(self)
     }
+
+    async fn register_as_slave(&mut self, server_id: u32) -> Result<()> {
+        use mysql_common::packets::ComRegisterSlave;
+
+        self.query_drop("SET @master_binlog_checksum='ALL'").await?;
+
+        let cmd = ComRegisterSlave::new(server_id);
+        let mut buf = Vec::with_capacity(cmd.len());
+        cmd.write(&mut buf)?;
+        self.write_command_raw(buf).await?;
+
+        // Server will respond with OK.
+        self.read_packet().await?;
+
+        Ok(())
+    }
+
+    async fn request_binlog(&mut self, request: BinlogRequest) -> Result<()> {
+        self.register_as_slave(request.server_id()).await?;
+        let cmd_buf = request.serialize_cmd();
+        self.write_command_raw(cmd_buf).await?;
+        Ok(())
+    }
+
+    pub async fn get_binlog_stream(
+        mut self,
+        request: BinlogRequest,
+    ) -> Result<
+        Pin<Box<dyn futures_core::stream::Stream<Item = Result<mysql_common::binlog::Event>>>>,
+    > {
+        use futures_util::stream;
+        use mysql_common::binlog::{BinlogVersion::Version4, EventStreamReader};
+
+        // We'll disconnect this connection from a pool.
+        self.inner.pool = None;
+        self.request_binlog(request).await?;
+
+        enum State {
+            Iterating(Conn, EventStreamReader),
+            Done,
+        }
+
+        let state = State::Iterating(self, EventStreamReader::new(Version4));
+
+        let event_stream = stream::unfold(state, |state| async move {
+            loop {
+                match state {
+                    State::Iterating(mut conn, mut esr) => match conn.read_packet().await {
+                        Ok(packet) if packet.get(0) == Some(&0) => {
+                            let event_data = &packet[1..];
+                            match esr.read(event_data) {
+                                Ok(event) => return Some((Ok(event), State::Iterating(conn, esr))),
+                                Err(err) => return Some((Err(err.into()), State::Done)),
+                            }
+                        }
+                        Ok(packet) if packet.get(0) == Some(&254) => {
+                            let eof_packet = mysql_common::packets::parse_ok_packet(
+                                &packet,
+                                conn.capabilities(),
+                                OkPacketKind::NetworkStreamTerminator,
+                            );
+                            match eof_packet {
+                                Ok(_) => return None,
+                                Err(_) => {
+                                    return Some((
+                                        Err(DriverError::UnexpectedPacket { payload: packet }
+                                            .into()),
+                                        State::Done,
+                                    ))
+                                }
+                            }
+                        }
+                        Ok(packet) => {
+                            return Some((
+                                Err(DriverError::UnexpectedPacket { payload: packet }.into()),
+                                State::Done,
+                            ))
+                        }
+                        Err(err) => return Some((Err(err), State::Done)),
+                    },
+                    State::Done => return None,
+                }
+            }
+        });
+
+        Ok(Box::pin(event_stream))
+    }
+}
+
+/// Binlog request representation. Please consult MySql documentation.
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct BinlogRequest {
+    /// Server id of a slave.
+    server_id: u32,
+    /// If true, then `COM_BINLOG_DUMP_GTID` will be used.
+    use_gtid: bool,
+    /// If `use_gtid` is `false`, then all flags except `BINLOG_DUMP_NON_BLOCK` will be truncated.
+    flags: BinlogDumpFlags,
+    /// Filename of the binlog on the master.
+    filename: Vec<u8>,
+    /// Position in the binlog-file to start the stream with.
+    ///
+    /// If `use_gtid` is `false`, then the value will be truncated to u32.
+    pos: u64,
+    /// SID blocks. If `use_gtid` is `false`, then this value is ignored.
+    sid_blocks: Vec<SidBlock>,
+}
+
+impl BinlogRequest {
+    /// Creates new request with the given slave server id.
+    pub fn new(server_id: u32) -> Self {
+        Self {
+            server_id,
+            use_gtid: false,
+            flags: BinlogDumpFlags::empty(),
+            filename: Vec::new(),
+            pos: 4,
+            sid_blocks: vec![],
+        }
+    }
+
+    /// Server id of a slave.
+    pub fn server_id(&self) -> u32 {
+        self.server_id
+    }
+
+    /// If true, then `COM_BINLOG_DUMP_GTID` will be used (defaults to `false`).
+    pub fn use_gtid(&self) -> bool {
+        self.use_gtid
+    }
+
+    /// If `use_gtid` is `false`, then all flags except `BINLOG_DUMP_NON_BLOCK` will be truncated
+    /// (defaults to empty).
+    pub fn flags(&self) -> BinlogDumpFlags {
+        self.flags
+    }
+
+    /// Filename of the binlog on the master (defaults to an empty string).
+    pub fn filename(&self) -> &[u8] {
+        &self.filename
+    }
+
+    /// Position in the binlog-file to start the stream with (defaults to `4`).
+    ///
+    /// If `use_gtid` is `false`, then the value will be truncated to u32.
+    pub fn pos(&self) -> u64 {
+        self.pos
+    }
+
+    /// If `use_gtid` is `false`, then this value will be ignored (defaults to an empty vector).
+    pub fn sid_blocks(&self) -> &[SidBlock] {
+        &self.sid_blocks
+    }
+
+    /// Returns modified `self` with the given value of the `server_id` field.
+    pub fn with_server_id(mut self, server_id: u32) -> Self {
+        self.server_id = server_id;
+        self
+    }
+
+    /// Returns modified `self` with the given value of the `use_gtid` field.
+    pub fn with_use_gtid(mut self, use_gtid: bool) -> Self {
+        self.use_gtid = use_gtid;
+        self
+    }
+
+    /// Returns modified `self` with the given value of the `flags` field.
+    pub fn with_flags(mut self, flags: BinlogDumpFlags) -> Self {
+        self.flags = flags;
+        self
+    }
+
+    /// Returns modified `self` with the given value of the `filename` field.
+    pub fn with_filename<T: Into<Vec<u8>>>(mut self, filename: T) -> Self {
+        self.filename = filename.into();
+        self
+    }
+
+    /// Returns modified `self` with the given value of the `pos` field.
+    pub fn with_pos<T: Into<u64>>(mut self, pos: T) -> Self {
+        self.pos = pos.into();
+        self
+    }
+
+    /// Returns modified `self` with the given value of the `sid_blocks` field.
+    pub fn with_sid_blocks<T>(mut self, sid_blocks: T) -> Self
+    where
+        T: IntoIterator<Item = SidBlock>,
+    {
+        self.sid_blocks = sid_blocks.into_iter().collect();
+        self
+    }
+
+    fn serialize_cmd(self) -> Vec<u8> {
+        macro_rules! to_buf {
+            ($cmd:ident) => {{
+                let mut buf = vec![0_u8; $cmd.len()];
+                $cmd.write(&mut buf[..]).expect("unreachable");
+                buf
+            }};
+        }
+
+        if self.use_gtid() {
+            let cmd = ComBinlogDumpGtid::new(self.server_id)
+                .with_pos(self.pos)
+                .with_flags(self.flags)
+                .with_filename(self.filename)
+                .with_sid_blocks(self.sid_blocks);
+            to_buf!(cmd)
+        } else {
+            let cmd = ComBinlogDump::new(self.server_id)
+                .with_pos(self.pos as u32)
+                .with_filename(self.filename)
+                .with_flags(self.flags & BinlogDumpFlags::BINLOG_DUMP_NON_BLOCK);
+            to_buf!(cmd)
+        }
+    }
 }
 
 #[cfg(test)]
 mod test {
+    use futures_util::stream::StreamExt;
+    use tokio::time::timeout;
+
+    use std::time::Duration;
+
     use crate::{
-        from_row, params, prelude::*, test_misc::get_opts, Conn, Error, OptsBuilder, Pool, TxOpts,
-        WhiteListFsLocalInfileHandler,
+        from_row, params, prelude::*, test_misc::get_opts, BinlogDumpFlags, BinlogRequest, Conn,
+        Error, OptsBuilder, Pool, TxOpts, WhiteListFsLocalInfileHandler,
     };
+
+    async fn gen_dummy_data() -> super::Result<()> {
+        let mut conn = Conn::new(get_opts()).await?;
+
+        "CREATE TABLE IF NOT EXISTS customers (customer_id int not null)"
+            .ignore(&mut conn)
+            .await?;
+
+        for i in 0_u8..100 {
+            "INSERT INTO customers(customer_id) VALUES (?)"
+                .with((i,))
+                .ignore(&mut conn)
+                .await?;
+        }
+
+        "DROP TABLE customers".ignore(&mut conn).await?;
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn should_read_binlog() -> super::Result<()> {
+        async fn get_conn() -> super::Result<(Conn, String, u64)> {
+            let mut conn = Conn::new(get_opts()).await?;
+            let (filename, position, _enc): (_, _, crate::Value) =
+                "SHOW BINARY LOGS".first(&mut conn).await?.unwrap();
+            gen_dummy_data().await?;
+            Ok((conn, filename, position))
+        }
+
+        // iterate using COM_BINLOG_DUMP
+        let (conn, filename, pos) = get_conn().await?;
+
+        let mut binlog_stream = conn
+            .get_binlog_stream(BinlogRequest::new(12).with_filename(filename).with_pos(pos))
+            .await?;
+
+        while let Ok(Some(event)) = timeout(Duration::from_secs(1), binlog_stream.next()).await {
+            let event = event?;
+            event.header.event_type.get().unwrap();
+            event.read_data()?;
+        }
+
+        // iterate using COM_BINLOG_DUMP_GTID
+        let (conn, filename, pos) = get_conn().await?;
+
+        let mut binlog_stream = conn
+            .get_binlog_stream(
+                BinlogRequest::new(12)
+                    .with_use_gtid(true)
+                    .with_filename(filename)
+                    .with_pos(pos),
+            )
+            .await?;
+
+        while let Ok(Some(event)) = timeout(Duration::from_secs(1), binlog_stream.next()).await {
+            let event = event?;
+            event.header.event_type.get().unwrap();
+            event.read_data()?;
+        }
+
+        // iterate using COM_BINLOG_DUMP with BINLOG_DUMP_NON_BLOCK flag
+        let (conn, filename, pos) = get_conn().await?;
+
+        let mut binlog_stream = conn
+            .get_binlog_stream(
+                BinlogRequest::new(12)
+                    .with_filename(filename)
+                    .with_pos(pos)
+                    .with_flags(BinlogDumpFlags::BINLOG_DUMP_NON_BLOCK),
+            )
+            .await?;
+
+        while let Some(event) = binlog_stream.next().await {
+            let event = event?;
+            event.header.event_type.get().unwrap();
+            event.read_data()?;
+        }
+
+        Ok(())
+    }
 
     #[test]
     fn opts_should_satisfy_send_and_sync() {
