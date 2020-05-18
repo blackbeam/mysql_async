8ad0da31abd3076eeff5e936212c2555c4206ad5
Anatoly Ikorsky <aikorsky@gmail.com>
2020-05-18 16:45:03 +0300

    Introduce BinlogStream structure

--- src/lib.rs
+++ src/lib.rs
@@ -122,7 +122,10 @@ static BUFFER_POOL: once_cell::sync::Lazy<Arc<crate::buffer_pool::BufferPool>> =
     once_cell::sync::Lazy::new(|| Default::default());
 
 #[doc(inline)]
-pub use self::conn::{BinlogRequest, Conn};
+pub use self::conn::{
+    binlog_stream::{BinlogRequest, BinlogStream},
+    Conn,
+};
 
 #[doc(inline)]
 pub use self::conn::pool::Pool;
