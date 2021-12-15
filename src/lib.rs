9531dc11d0ba22303514a504308398cb970bb253
Anatoly Ikorsky <aikorsky@gmail.com>
2021-12-15 18:01:05 +0300

    Remove mysql_common deps reexports

--- src/lib.rs
+++ src/lib.rs
@@ -91,7 +91,7 @@
 #[cfg(feature = "nightly")]
 extern crate test;
 
-pub use mysql_common::{constants as consts, params, time03, uuid};
+pub use mysql_common::{constants as consts, params};
 
 use std::sync::Arc;
 
--- tests/exports.rs
+++ tests/exports.rs
@@ -1,15 +1,15 @@
 #[allow(unused_imports)]
 use mysql_async::{
-    chrono, consts, from_row, from_row_opt, from_value, from_value_opt,
+    consts, from_row, from_row_opt, from_value, from_value_opt,
     futures::{DisconnectPool, GetConn},
     params,
     prelude::{
         BatchQuery, ConvIr, FromRow, FromValue, LocalInfileHandler, Protocol, Query, Queryable,
         StatementLike, ToValue,
     },
-    time, uuid, BinaryProtocol, Column, Conn, Deserialized, DriverError, Error, FromRowError,
-    FromValueError, IoError, IsolationLevel, Opts, OptsBuilder, Params, ParseError, Pool,
-    PoolConstraints, PoolOpts, QueryResult, Result, Row, Serialized, ServerError, SslOpts,
-    Statement, TextProtocol, Transaction, TxOpts, UrlError, Value, WhiteListFsLocalInfileHandler,
+    BinaryProtocol, Column, Conn, Deserialized, DriverError, Error, FromRowError, FromValueError,
+    IoError, IsolationLevel, Opts, OptsBuilder, Params, ParseError, Pool, PoolConstraints,
+    PoolOpts, QueryResult, Result, Row, Serialized, ServerError, SslOpts, Statement, TextProtocol,
+    Transaction, TxOpts, UrlError, Value, WhiteListFsLocalInfileHandler,
     DEFAULT_INACTIVE_CONNECTION_TTL, DEFAULT_TTL_CHECK_INTERVAL,
 };
