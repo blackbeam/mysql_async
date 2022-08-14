38a77398b4f9eb779ae93a3c9773e69d4eb02b2b
Tobias de Bruijn <t.debruijn@array21.dev>
2022-08-14 12:46:31 +0200

    Fixed misplaced )

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -153,7 +153,7 @@ impl Future for CheckTcpStream<'_> {
 }
 
 impl Endpoint {
-    #[cfg(all(any(feature = "native-tls-tls", feature = "rustls-tls")), unix)]
+    #[cfg(all(any(feature = "native-tls-tls", feature = "rustls-tls"), unix))]
     fn is_socket(&self) -> bool {
         match self {
             Self::Socket(_) => true,
